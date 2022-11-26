5b553442d091088323dfcded8db558ff80af36a7
joavitormartins <99548321+joavitormartins@users.noreply.github.com>
2022-11-26 11:40:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+document.querySelector('.for').addEventListener('submit',(event) => {
+  event.preventDefault()
+
+
+    const input = document.querySelector(".CPF").value;
+    /*se TestaCPF(input) == true*/
+    if(TestaCPF(input)){
+      document.querySelector(".resultado").innerHTML = "CFP aprovado"
+    }else{
+      document.querySelector(".resultado").innerHTML = "CFP invalido"
+    }
+
+})
+
+
+
+
+
+
+
+
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
